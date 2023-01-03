f72e73da88e3374a9b1d43da8efb0b2fdd11ccb8
91song4 <91song4@daum.net>
2023-01-03 14:11:15 +0900

    feat:포스트 조회

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const {createPosts} = require("../controllers/post.controller")
+const { createPosts, getPosts } = require("../controllers/post.controller")
 
 router.post("/posts", createPosts);
+router.get("/posts", getPosts);
 
 module.exports = router;
